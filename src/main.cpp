5c4f4218896d87d2acf99cb7dadb12e2dad8e342
irog-c <mihajlov.igor15@gmail.com>
2024-05-28 22:15:08 +0200

    Small changes

--- src/main.cpp
+++ src/main.cpp
@@ -68,15 +68,15 @@ static auto enrich_viewer_with_intersections(
 	auto left_hip = joints[left_hip_idx];
 	auto right_hip = joints[right_hip_idx];
 
-	pcl::PointXYZ shoulder_midpoint(
+	auto shoulder_midpoint = pcl::PointXYZ(
 	    (left_shoulder.x + right_shoulder.x) / 2,
 	    (left_shoulder.y + right_shoulder.y) / 2,
 	    (left_shoulder.z + right_shoulder.z) / 2);
-	pcl::PointXYZ hip_midpoint(
+	auto hip_midpoint = pcl::PointXYZ(
 	    (left_hip.x + right_hip.x) / 2,
 	    (left_hip.y + right_hip.y) / 2,
 	    (left_hip.z + right_hip.z) / 2);
-	pcl::PointXYZ shoulder_hip_midpoint(
+	auto shoulder_hip_midpoint = pcl::PointXYZ(
 	    (hip_midpoint.x + shoulder_midpoint.x) / 2,
 	    (hip_midpoint.y + shoulder_midpoint.y) / 2,
 	    (hip_midpoint.z + shoulder_midpoint.z) / 2);
@@ -86,16 +86,16 @@ static auto enrich_viewer_with_intersections(
 	viewer->addSphere(shoulder_hip_midpoint, 0.01, 0, 255, 0, "intersection_point");
 	viewer->addLine<pcl::PointXYZ>(hip_midpoint, shoulder_midpoint, "midline");
 
-	float avg_y = (left_shoulder.y + right_shoulder.y + left_hip.y + right_hip.y) / 4.0;
-	float avg_x = (left_shoulder.x + right_shoulder.x + left_hip.x + right_hip.x) / 4.0;
-	pcl::PointCloud<pcl::PointXYZ>::Ptr intersection_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+	auto avg_y = (left_shoulder.y + right_shoulder.y + left_hip.y + right_hip.y) / 4.0;
+	auto avg_x = (left_shoulder.x + right_shoulder.x + left_hip.x + right_hip.x) / 4.0;
+	auto intersection_cloud = pcl::PointCloud<pcl::PointXYZ>::Ptr(new pcl::PointCloud<pcl::PointXYZ>);
 	for (const auto& point : cloud->points) {
 		if (std::abs(point.y - avg_y) < 0.01 && std::abs(point.x - avg_x) < 0.01) {
 			intersection_cloud->points.push_back(point);
 		}
 	}
 
-	pcl::PointXYZ min_z_point = *std::min_element(intersection_cloud->points.begin(), intersection_cloud->points.end(), [](const auto& a, const auto& b) {
+	auto min_z_point = *std::min_element(intersection_cloud->points.begin(), intersection_cloud->points.end(), [](const auto& a, const auto& b) {
 		return a.z < b.z;
 	});
 
